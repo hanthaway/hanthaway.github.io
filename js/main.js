397c5b9579c9f1b9b5bdd75abd2efd2c19e55813
hanthaway <hanthaway@sina.com>
2016-03-28 23:35:53 +0800

    log

--- js/main.js
+++ js/main.js
@@ -54,41 +54,7 @@
     $('.show-menu a').click(function() {
     	$('.show-menu').fadeOut('slow');
     });
-    var oList=document.getElementById('list');
-    var aBtn=document.getElementById('list_btn').children;
-    for (var i = 0; i < 6; i++) {
-    	var oDiv=document.createElement('div');
-    	oDiv.className="col-md-4 col-sm-6 col-xs-12";
-    	oDiv.innerHTML='<div class="project-item">\
-                                            <img src="images/'+(i+1)+'.jpg" alt="">\
-                                            <div class="project-hover">\
-                                                <h4>Great Nature Capture</h4>\
-                                            </div>\
-                                        </div>';
-         oList.appendChild(oDiv);
-    };
-    var aPos=[];
-    var aLi=oList.children;
-	// for(var i=0;i<aLi.length;i++){
-	// 	aPos.push({
-	// 		left:aLi[i].offsetLeft,
-	// 		top:aLi[i].offsetTop,
-	// 		width:aLi[i].offsetWidth,
-	// 		height:aLi[i].offsetHeight,
-	// 		opacity:1
-	// 		});	
-	// 	aLi[i].style.left=aPos[i].left+'px';
-	// 	aLi[i].style.top=aPos[i].top+'px';
-	// }
-	// for(var i=0;i<aLi.length;i++){
-	// 	aLi[i].style.position='absolute';	
-	// 	aLi[i].style.margin=0;
-	// }
-    for (var i = 0; i < aBtn.length; i++) {
-    	aBtn[i].onclick=function(){
-    		alert();
-    	}
-    };
+ 
 
 
 })(jQuery);
